ea0f66aed6ffdc885af51878434a3dfcc52aa2fc
rickyfbrianto <71504886+rickyfbrianto@users.noreply.github.com>
2024-05-17 18:59:10 +0800

    2024-05-17

--- app/(app)/ibadah/[id].js
+++ app/(app)/ibadah/[id].js
@@ -1,10 +1,10 @@
-import { View, Text, SafeAreaView, Pressable, ActivityIndicator, Image, ScrollView, RefreshControl } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { View, Text, SafeAreaView, Pressable, ActivityIndicator, Image, ScrollView, RefreshControl, FlatList } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useFocusEffect, useLocalSearchParams } from 'expo-router'
 import { COLORS } from '../../../constants/Colors'
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
 import { router } from 'expo-router'
-import { FontAwesome, Feather } from '@expo/vector-icons';
+import { FontAwesome, MaterialCommunityIcons, MaterialIcons, Feather, Entypo } from '@expo/vector-icons';
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { doc, getDoc } from 'firebase/firestore'
 import { db } from '../../../firebaseConfig'
@@ -35,8 +35,15 @@ const IbadahDetail = () => {
         }, 50)
     }
 
+    useFocusEffect(
+        useCallback(() => {
+            console.log(dataQuery.data)
+            dataQuery.refetch()
+        }, [])
+    )
+
     return (
-        <ScrollView refreshControl={<RefreshControl refreshing={refresh} onRefresh={handleRefresh} />} style={{ flex: 1, backgroundColor: "white" }}>
+        <ScrollView nestedScrollEnabled refreshControl={<RefreshControl refreshing={refresh} onRefresh={handleRefresh} />} style={{ flex: 1, backgroundColor: "white" }}>
             <View style={{ backgroundColor: colorBase, flex: 1 }}>
                 <SafeAreaView style={{ paddingTop: top + 10, flexDirection: 'row', marginHorizontal: hp(2) }}>
                     <Pressable onPress={() => router.back()}>
@@ -45,36 +52,144 @@ const IbadahDetail = () => {
                 </SafeAreaView>
                 {dataQuery.isLoading
                     ? <Text>Loading...</Text>
-                    : <View className="flex-1 items-center bg-white" style={{ borderTopLeftRadius: hp(6), flex: 1, borderTopRightRadius: hp(6), marginTop: 100, paddingTop: 20, }}>
+                    : <View className="flex-1 items-center bg-white" style={{ borderTopLeftRadius: hp(6), flex: 1, borderTopRightRadius: hp(6), marginTop: 100, paddingBottom: 20, }}>
                         <Image source={require('../../../assets/images/welcome.jpg')} style={{ top: hp(-10), height: hp(20), width: wp(40), resizeMode: "stretch", position: "absolute", borderRadius: 20 }} />
                         <View className="w-full" style={{ marginTop:hp(10)}}>
                             <Text style={{ fontFamily: "outfit-bold", fontSize: hp(3) }}>{dataQuery.data.judul}</Text>
                         </View>
                         <View className="flex-1 justify-center" style={{ marginTop: hp(2), width: "100%", paddingHorizontal: hp(2) }}>
-                            <View style={{ flexDirection: "row", alignItems: "center", borderBottomWidth: 2, borderBottomColor: colorBase, columnGap: hp(2), marginHorizontal: hp(1), paddingHorizontal: hp(1), paddingVertical: hp(2) }}>
+                            <View style={{ flexDirection: "row", alignItems: "center", borderBottomWidth: 1, borderBottomColor: colorBase, columnGap: hp(2), marginHorizontal: hp(1), paddingHorizontal: hp(1), paddingVertical: hp(1.4) }}>
                                 <View style={{ flexDirection: "row", justifyContent: "center", width: 30 }}>
-                                    <FontAwesome name="location-arrow" size={24} color="black" />
+                                    <FontAwesome name="location-arrow" size={14} color="black" />
                                 </View>
-                                <Text>{dataQuery.data.lokasi}</Text>
+                                <Text style={{fontSize:hp(1.8)}}>{dataQuery.data.lokasi}</Text>
                             </View>
-                            <View style={{ flexDirection: "row", alignItems: "center", borderBottomWidth: 2, borderBottomColor: colorBase, columnGap: hp(2), marginHorizontal: hp(1), paddingHorizontal: hp(1), paddingVertical: hp(2) }}>
+                            <View style={{ flexDirection: "row", alignItems: "center", borderBottomWidth: 1, borderBottomColor: colorBase, columnGap: hp(2), marginHorizontal: hp(1), paddingHorizontal: hp(1), paddingVertical: hp(1.4) }}>
                                 <View style={{ flexDirection: "row", justifyContent: "center", width: 30 }}>
-                                    <FontAwesome name="user" size={24} color="black" />
+                                    <FontAwesome name="user" size={14} color="black" />
                                 </View>
-                                <Text>{dataQuery.data.pdt}</Text>
+                                <Text style={{fontSize:hp(1.8)}}>{dataQuery.data.pdt}</Text>
                             </View>
-                            <View style={{ flexDirection: "row", alignItems: "center", borderBottomWidth: 2, borderBottomColor: colorBase, columnGap: hp(2), marginHorizontal: hp(1), paddingHorizontal: hp(1), paddingVertical: hp(2) }}>
+                            <View style={{ flexDirection: "row", alignItems: "center", borderBottomWidth: 1, borderBottomColor: colorBase, columnGap: hp(2), marginHorizontal: hp(1), paddingHorizontal: hp(1), paddingVertical: hp(1.4) }}>
                                 <View style={{ flexDirection: "row", justifyContent: "center", width: 30 }}>
-                                    <FontAwesome name="calendar-o" size={24} color="black" />
+                                    <FontAwesome name="calendar-o" size={14} color="black" />
                                 </View>
-                                <Text>{dataQuery.data.hari}</Text>
+                                <Text style={{fontSize:hp(1.8)}}>{dataQuery.data.hari}</Text>
                             </View>
-                            <View style={{ flexDirection: "row", alignItems: "center", borderBottomWidth: 2, borderBottomColor: colorBase, columnGap: hp(2), marginHorizontal: hp(1), paddingHorizontal: hp(1), paddingVertical: hp(2) }}>
+                            <View style={{ flexDirection: "row", alignItems: "center", borderBottomWidth: 1, borderBottomColor: colorBase, columnGap: hp(2), marginHorizontal: hp(1), paddingHorizontal: hp(1), paddingVertical: hp(1.4) }}>
                                 <View style={{ flexDirection: "row", justifyContent: "center", width: 30 }}>
-                                    <Feather name="clock" size={24} color="black" />
+                                    <Feather name="clock" size={14} color="black" />
                                 </View>
-                                <Text>{dataQuery.data.jam}</Text>
+                                <Text style={{fontSize:hp(1.8)}}>{dataQuery.data.jam}</Text>
                             </View>
+
+                            {/* WL */}
+                            {dataQuery.data.wl[0] && 
+                                <View style={{ borderBottomWidth: 1, borderBottomColor: colorBase, marginHorizontal: hp(1), paddingHorizontal: hp(1), paddingVertical: hp(1.4) }}>
+                                    <View style={{flexDirection:"row", marginBottom:10, columnGap: hp(2), alignItems:"center"}}>
+                                        <View style={{ flexDirection: "row", justifyContent: "center", width: 30 }}>
+                                            <MaterialCommunityIcons name="microphone-variant" size={14} color="gray" />
+                                        </View>
+                                        <Text style={{fontSize:hp(1.8)}}>Worship Leader</Text>
+                                    </View>
+
+                                    <FlatList data={dataQuery.data.wl} numColumns={2} contentContainerStyle={{rowGap:10, display:"flex"}} columnWrapperStyle={{columnGap:10}} renderItem={({item, index})=>(
+                                        <View style={{flex:1, paddingHorizontal:10, paddingVertical:6, backgroundColor:"whitesmoke"}}>
+                                            <Text>{index+1 + ". " + item.value}</Text>
+                                        </View>
+                                    )}/>
+                                </View>
+                            }
+
+                            {/* Singer */}
+                            {dataQuery.data.singer[0] && 
+                                <View style={{ borderBottomWidth: 1, borderBottomColor: colorBase, marginHorizontal: hp(1), paddingHorizontal: hp(1), paddingVertical: hp(1.4) }}>
+                                    <View style={{flexDirection:"row", marginBottom:10, columnGap: hp(2), alignItems:"center"}}>
+                                        <View style={{ flexDirection: "row", justifyContent: "center", width: 30 }}>
+                                            <Entypo name="modern-mic" size={14} color="gray" />
+                                        </View>
+                                        <Text style={{fontSize:hp(1.8)}}>Singer</Text>
+                                    </View>
+
+                                    <FlatList data={dataQuery.data.singer} numColumns={2} contentContainerStyle={{rowGap:10, display:"flex"}} columnWrapperStyle={{columnGap:10}} renderItem={({item, index})=>(
+                                        <View style={{flex:1, paddingHorizontal:10, paddingVertical:6, backgroundColor:"whitesmoke"}}>
+                                            <Text>{index+1 + ". " + item.value}</Text>
+                                        </View>
+                                    )}/>
+                                </View>
+                            }
+
+                            {/* Musik */}
+                            {dataQuery.data.musik[0] && 
+                            <View style={{ borderBottomWidth: 1, borderBottomColor: colorBase, marginHorizontal: hp(1), paddingHorizontal: hp(1), paddingVertical: hp(1.4) }}>
+                                <View style={{flexDirection:"row", marginBottom:10, columnGap: hp(2), alignItems:"center"}}>
+                                    <View style={{ flexDirection: "row", justifyContent: "center", width: 30 }}>
+                                        <MaterialCommunityIcons name="music" size={14} color="gray" />
+                                    </View>
+                                    <Text style={{fontSize:hp(1.8)}}>Pemain Musik</Text>
+                                </View>
+
+                                <FlatList data={dataQuery.data.musik} numColumns={2} contentContainerStyle={{rowGap:10, display:"flex"}} columnWrapperStyle={{columnGap:10}} renderItem={({item, index})=>(
+                                    <View style={{flex:1, paddingHorizontal:10, paddingVertical:6, backgroundColor:"whitesmoke"}}>
+                                        <Text>{index+1 + ". " + item.value}</Text>
+                                    </View>
+                                )}/>
+                            </View>
+                            }
+
+                            {/* MM */}
+                            {dataQuery.data.mm[0] && 
+                                <View style={{ borderBottomWidth: 1, borderBottomColor: colorBase, marginHorizontal: hp(1), paddingHorizontal: hp(1), paddingVertical: hp(1.4) }}>
+                                    <View style={{flexDirection:"row", marginBottom:10, columnGap: hp(2), alignItems:"center"}}>
+                                        <View style={{ flexDirection: "row", justifyContent: "center", width: 30 }}>
+                                            <Feather name="monitor" size={14} color="gray" />
+                                        </View>
+                                        <Text style={{fontSize:hp(1.8)}}>Multimedia</Text>
+                                    </View>
+
+                                    <FlatList data={dataQuery.data.mm} numColumns={2} contentContainerStyle={{rowGap:10, display:"flex"}} columnWrapperStyle={{columnGap:10}} renderItem={({item, index})=>(
+                                        <View style={{flex:1, paddingHorizontal:10, paddingVertical:6, backgroundColor:"whitesmoke"}}>
+                                            <Text>{index+1 + ". " + item.value}</Text>
+                                        </View>
+                                    )}/>
+                                </View>
+                            }
+                            
+                            {/* Protokoler */}
+                            {dataQuery.data.protokoler[0] && 
+                                <View style={{ borderBottomWidth: 1, borderBottomColor: colorBase, marginHorizontal: hp(1), paddingHorizontal: hp(1), paddingVertical: hp(1.4) }}>
+                                    <View style={{flexDirection:"row", marginBottom:10, columnGap: hp(2), alignItems:"center"}}>
+                                        <View style={{ flexDirection: "row", justifyContent: "center", width: 30 }}>
+                                            <MaterialIcons name="supervised-user-circle" size={14} color="gray" />
+                                        </View>
+                                        <Text style={{fontSize:hp(1.8)}}>Protokoler</Text>
+                                    </View>
+
+                                    <FlatList data={dataQuery.data.protokoler} numColumns={2} contentContainerStyle={{rowGap:10, display:"flex"}} columnWrapperStyle={{columnGap:10}} renderItem={({item, index})=>(
+                                        <View style={{flex:1, paddingHorizontal:10, paddingVertical:6, backgroundColor:"whitesmoke"}}>
+                                            <Text>{index+1 + ". " + item.value}</Text>
+                                        </View>
+                                    )}/>
+                                </View>
+                            }
+
+                            {/* TBF */}
+                            {dataQuery.data.tbf[0] && 
+                                <View style={{ borderBottomWidth: 1, borderBottomColor: colorBase, marginHorizontal: hp(1), paddingHorizontal: hp(1), paddingVertical: hp(1.4) }}>
+                                    <View style={{flexDirection:"row", marginBottom:10, columnGap: hp(2), alignItems:"center"}}>
+                                        <View style={{ flexDirection: "row", justifyContent: "center", width: 30 }}>
+                                            <FontAwesome name="user-circle-o" size={14} color="gray" />
+                                        </View>
+                                        <Text style={{fontSize:hp(1.8)}}>TBF</Text>
+                                    </View>
+
+                                    <FlatList data={dataQuery.data.tbf} numColumns={2} contentContainerStyle={{rowGap:10, display:"flex"}} columnWrapperStyle={{columnGap:10}} renderItem={({item, index})=>(
+                                        <View style={{flex:1, paddingHorizontal:10, paddingVertical:6, backgroundColor:"whitesmoke"}}>
+                                            <Text>{index+1 + ". " + item.value}</Text>
+                                        </View>
+                                    )}/>
+                                </View>
+                            }
                         </View>
                     </View>
                 }
